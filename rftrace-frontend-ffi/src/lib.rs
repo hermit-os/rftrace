9233c74bf16f0639a2379edc0dec0e9111e8f7ba
Martin Kröning <mkroening@posteo.net>
2021-07-14 14:37:23 +0200

    Fix clippy::redundant_pattern_matching

--- rftrace-frontend-ffi/src/lib.rs
+++ rftrace-frontend-ffi/src/lib.rs
@@ -40,7 +40,7 @@ pub unsafe extern "C" fn rftrace_dump_full_uftrace(
     let binary_name = CStr::from_ptr(binary_name).to_string_lossy().into_owned();
     let linux = linux_mode != 0;
 
-    if let Err(_) = rftrace_frontend::dump_full_uftrace(&mut *events, &out_dir, &binary_name, linux)
+    if rftrace_frontend::dump_full_uftrace(&mut *events, &out_dir, &binary_name, linux).is_err()
     {
         return -1;
     }
@@ -52,7 +52,7 @@ pub unsafe extern "C" fn rftrace_dump_full_uftrace(
 pub unsafe extern "C" fn rftrace_dump_trace(events: *mut Events, outfile: *const c_char) -> i64 {
     let outfile = CStr::from_ptr(outfile).to_string_lossy().into_owned();
 
-    if let Err(_) = rftrace_frontend::dump_trace(&mut *events, &outfile) {
+    if rftrace_frontend::dump_trace(&mut *events, &outfile).is_err() {
         return -1;
     }
     0
