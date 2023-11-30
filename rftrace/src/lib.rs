3fdbfc2e14c0d24ed9e44e7070928094ce38c62c
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-11-30 09:53:02 +0100

    feat(lib.rs): only enable nightly features for static library
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- rftrace/src/lib.rs
+++ rftrace/src/lib.rs
@@ -2,10 +2,10 @@
 //! Provides an `mcount` implementation, which does nothing by default but can be enabled via frontend.
 //! A lot of documentation can be found in the parent workspaces [readme](https://github.com/tlambertz/rftrace).
 
-#![feature(naked_functions)]
-#![feature(llvm_asm)]
-#![feature(thread_local)]
-#![feature(linkage)]
+#![cfg_attr(feature = "staticlib", feature(naked_functions))]
+#![cfg_attr(feature = "staticlib", feature(llvm_asm))]
+#![cfg_attr(feature = "staticlib", feature(thread_local))]
+#![cfg_attr(feature = "staticlib", feature(linkage))]
 #![cfg_attr(feature = "staticlib", no_std)]
 
 mod interface;
