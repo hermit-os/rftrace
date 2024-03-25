70d62585cbe53a8e670176230c7e198af4f776f2
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-03-25 16:35:30 +0100

    chore: change repo links
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- rftrace-frontend/src/lib.rs
+++ rftrace-frontend/src/lib.rs
@@ -1,6 +1,6 @@
 //! This crate provides a possible frontend for rftracer.
 //! It can initialize an event buffer, enable/disable tracing and save the trace to disk in a uftrace compatible format.
-//! A lot of documentation can be found in the parent workspaces [readme](https://github.com/tlambertz/rftrace).
+//! A lot of documentation can be found in the parent workspaces [readme](https://github.com/hermit-os/rftrace).
 
 #![feature(vec_into_raw_parts)]
 extern crate byteorder;
